511bb789bc843662e9d09c3c93f8d93f312284b7
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-07 15:16:47 +0900

    rails g controller StaticPages terms privacy contactコマンド実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   root 'top#index'
+
+  get "/terms", to: "static_pages#terms", as: "terms"
+  get "/privacy", to: "static_pages#privacy", as: "privacy"
+  get "/contact", to: "static_pages#contact", as: "contact"
 end

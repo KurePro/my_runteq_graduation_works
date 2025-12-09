27c02f21f2b32e7f8ea4b840c1ec00a0f26b45c2
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-10 00:32:15 +0900

    add:Userモデルにバリデーションとアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  has_many :foods, dependent: :destroy
+  has_many :categories, dependent: :destroy
+  has_many :shopping_items, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 end

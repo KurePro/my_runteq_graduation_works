8b80f66192a3c4bdbe0b0e13c9995c4dfd8ce8cd
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-20 16:06:26 +0900

    chore: アプリのタイムゾーンをAsia/Tokyoに設定

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module App
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
 

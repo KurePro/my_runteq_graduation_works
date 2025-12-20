f15a79e4990e8d51cb23b1a96f41c9ef5f948525
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-20 16:05:51 +0900

    feat: 通知一覧・削除用コントローラーを追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,12 @@
 class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.notifications.order(created_at: :desc)
+    @notifications.where(checked: false).update_all(checked: true)
+  end
+
+  def destroy
+    notification = current_user.notifications.find(params[:id])
+    notification.destroy
+    redirect_to notifications_path, status: :see_other
+  end
 end

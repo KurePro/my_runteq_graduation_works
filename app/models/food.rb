4ab8dcfc9d1aff9e9cd2d60ecd39035f07feede1
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-20 16:04:34 +0900

    feat: 賞味期限通知用のスコープをFoodに追加

--- app/models/food.rb
+++ app/models/food.rb
@@ -5,4 +5,8 @@ class Food < ApplicationRecord
   belongs_to :user
   belongs_to :category, optional: true
   has_many :notifications, dependent: :destroy
+
+  scope :for_expiry_3_days_notice, -> { where(expiry_date: Date.current + 3.days) }
+  scope :for_expiry_day_notice, -> { where(expiry_date: Date.current) }
+  scope :for_expired_notice, -> { where("expiry_date < ?", Date.current) }
 end

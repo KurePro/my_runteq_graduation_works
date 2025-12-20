88647752a67a8c04b78f36afa27bce40a90729a5
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-20 15:57:47 +0900

    feat: 通知モデルを追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,5 +5,6 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :food
 
-  enum kind: { expiry_3_days: 0, expiry_day: 1, expired: 2 }
+  enum :kind, { expiry_3_days: 0, expiry_day: 1, expired: 2 }
+  # "enum kind:"だとRenderデプロイ時にエラーになるため、enumの書き方をRails8の推奨に合わせています
 end

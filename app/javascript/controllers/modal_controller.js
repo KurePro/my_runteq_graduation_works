5eadadfb42f1357e2385dd0e052a5b8ae56a862c
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-21 23:54:45 +0900

    add:modal用にコントローラー、ビュー、全体のHTML構成を調整

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,6 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="modal"
 export default class extends Controller {
   connect() {
-    this.element.showModal()
+    this.element.focus()
+  }
+  hide(event) {
+    event.preventDefault();
+
+    this.element.remove();
+  }
+
+  disconnect() {
+    this.#modalTurboFrame.src = null;
+  }
+
+  // private: 親のturbo-frameを探す便利メソッド?
+  get #modalTurboFrame() {
+    return document.querySelector("turbo-frame[id='modal']");
   }
 }

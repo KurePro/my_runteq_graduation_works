8b324a2304b15ef815217cef15a9f12d3aeb583d
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-26 00:20:59 +0900

    add:Users::SessionsController に guest_sign_in アクションを追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,9 @@
-class Users::SessionsController < ApplicationController
+class Users::SessionsController < Devise::SessionsController
+  skip_before_action :authenticate_user!, only: [:guest_sign_in]
+
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to foods_path, success: 'ゲストユーザーとしてログインしました。'
+  end
 end

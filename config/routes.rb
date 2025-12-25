880a3aafd54a62d269ab1756f3d1ca761bdf7b37
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-26 00:14:34 +0900

    add:ゲストログインのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
-  devise_for :users
+  devise_for :users, controller: {
+    registrations: 'users/registrations'
+  }
   resources :users, only: [:show]
   resources :foods
   resources :notifications, only: [:index] do
@@ -12,4 +14,8 @@ Rails.application.routes.draw do
   get "/terms", to: "static_pages#terms", as: "terms"
   get "/privacy", to: "static_pages#privacy", as: "privacy"
   get "/contact", to: "static_pages#contact", as: "contact"
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in', as: 'guest_sign_in'
+  end
 end

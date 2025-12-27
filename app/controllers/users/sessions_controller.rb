ac7987db14a2a1e3934e9acecc6f668da3a76737
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-27 16:34:43 +0900

    fix:ゲストログイン時にフラッシュメッセージが出ない問題に対応

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,6 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to foods_path, success: 'ゲストユーザーとしてログインしました。'
+    redirect_to foods_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end

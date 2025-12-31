e0be5aeab96e4fdb248c7aa4e425355510c2f2d8
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2025-12-31 16:46:51 +0900

    add:notificationコントローラーのindexアクションをpaginationに対応

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.notifications.order(created_at: :desc)
-    @notifications.where(checked: false).update_all(checked: true)
+    @notifications = current_user.notifications.order(created_at: :desc).page(params[:page]).per(5)
+    current_user.notifications.where(checked: false).update_all(checked: true)
   end
 
   def destroy_all

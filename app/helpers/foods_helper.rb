4475edf8309a25f9218db739ca937bba8de42b4a
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2026-02-23 23:13:00 +0900

    change:賞味期限ごとにカードの色を変更

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,8 @@
 module FoodsHelper
+  def food_card_color(food)
+    return 'border-l-default/80' if food.expiry_date.nil? || food.expiry_date > Date.current + 3.days
+    return 'border-l-expiry_3_days/80' if food.expiry_date > Date.current && food.expiry_date <= Date.current + 3.days
+    return 'border-l-expiry_day/80' if food.expiry_date == Date.current
+    'border-l-expired/80'
+  end
 end

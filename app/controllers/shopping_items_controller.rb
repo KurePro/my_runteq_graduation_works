5eaff200f563449bc270bce5040f9198bd4452cb
Moeno Kure <kure_pro@Mochans-MacBook-Air.local>
2026-03-06 01:02:37 +0900

    feat: 買い物リストの基本機能を実装

---
+++ app/controllers/shopping_items_controller.rb
@@ -0,0 +1,36 @@
+class ShoppingItemsController < ApplicationController
+  def index
+    @shopping_items = current_user.shopping_items.where(is_bought: false).order(created_at: :desc).page(params[:page]).per(12)
+
+    @shopping_item = ShoppingItem.new
+  end
+
+  def create
+    @shopping_item = current_user.shopping_items.build(shopping_item_params)
+    if @shopping_item.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to shopping_items_path, notice: "買い物リストに追加しました。" }
+      end
+    else
+      @shopping_items = current_user.shopping_items.order(created_at: :desc).page(params[:page]).per(12)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @shopping_item = current_user.shopping_items.find(params[:id])
+    @shopping_item.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to shopping_items_path, status: :see_other }
+    end
+  end
+
+  private
+
+  def shopping_item_params
+    params.require(:shopping_item).permit(:name, :memo)
+  end
+end
